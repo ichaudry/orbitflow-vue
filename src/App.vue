27152d1d2453b509e9ac9e6301b611def3590cdf
Ibraheem Chaudry <ibraheemchaudry2025@gmail.com>
2025-09-06 17:12:45 +0500

    Fixed gridding issue with sidebar.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="min-h-screen grid grid-cols-260px_1fr]">
+    <div class="min-h-screen grid grid-cols-[260px_1fr]">
         <SideBar />
-        
+
         <div class="flex flex-col">
             <NavBar />
             <main class="p-6">
