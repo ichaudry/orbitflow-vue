6b7987e86871886f43f806d9e4803dab3db39619
Ibraheem Chaudry <ibraheemchaudry2025@gmail.com>
2025-09-06 17:44:01 +0500

    Created separate env files for dev and production.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// Edit the base if deploying to a project page, e.g. '/orbitflow/'
-// For user/organization pages, leave base as '/'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
